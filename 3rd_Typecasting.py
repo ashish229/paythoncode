f6d49d94ae3128a1bf601d51aaf5a9319a113f44
ashish229 <72303789+ashish229@users.noreply.github.com>
2020-10-10 16:23:51 +0530

    Create 3rd_Typecasting.py

---
+++ 3rd_Typecasting.py
@@ -0,0 +1,9 @@
+'''var1 = "54"    # as a string 
+var2 = "32"    #as a string
+print(int(var1)+int(var2))'''
+print("Enter the first number to addition ")
+a = input()
+print("Enter the second number to addition ")
+b = input()
+c = int(a) + int(b)
+print("The sum of two number is :- ",c)
