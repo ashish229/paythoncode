46199f8687c6bf0ed4f27693b97f5d63bf0c7708
ashish229 <72303789+ashish229@users.noreply.github.com>
2020-10-11 12:30:25 +0530

    Create 6th_ifelse.py

---
+++ 6th_ifelse.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age : "))
+if age<0:
+      print("Please Enter vailed age")
+elif age>70:
+      print("Please Enter vailed age")
+elif age>18:
+      print("you are able to drive")
+elif age==18:
+       print("We will think about it ")
+else:
+      print("you cannot  drive")
