0ef60c9b8630b23f4677799842bf176d6d8c46d0
ashish229 <72303789+ashish229@users.noreply.github.com>
2020-10-12 13:49:47 +0530

    8th_continue_and_break.py
    This program is continue and break statement using While loop

---
+++ 8th_continew_brake.py
@@ -0,0 +1,8 @@
+i = 0
+while (True): #This condition is also true 
+      i = i+1 #Incress the value of  i 
+      if(i<5):
+            continue #The condition is true then loop is not exedute and condition is false the loop execute 
+      print(i)
+      if(i==44):
+            break #Stop the loop
