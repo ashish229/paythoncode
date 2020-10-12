568e8331c1d7c3fc8b3537db88686c6514d8847c
ashish229 <72303789+ashish229@users.noreply.github.com>
2020-10-12 14:09:40 +0530

    Create 9th_Quike_Quiz.py

---
+++ 9th_Quike_Quiz.py
@@ -0,0 +1,8 @@
+#Quki Quiz 
+# Q.} User enter the number program check the number are garter then 100 the condition are true then print Congratulations you Entered greater then 100 and condition false then replet the loop 
+i = 0
+while (True):
+      i = int(input("Enter the number :- "))
+      if(i>=100):
+            print("\nCongratulations you Entered greater then 100")
+            break 
