9c6b4b04c99936f1ebcc587572935b9378615a6e
ashish229 <72303789+ashish229@users.noreply.github.com>
2020-10-19 13:02:02 +0530

    Update and rename 10th_exersize.py to 10th_exersize_game1_guess_the_number.py

--- 10th_exersize_game1_guess_the_number.py
+++ 10th_exersize_game1_guess_the_number.py
@@ -1,21 +1,25 @@
 #This is a Game 
 #Guess the number Game
-print("            This game is present by Ashi Gameing \n\n                    \"Guess the number\"\n")
+import random 
+print("            This game is present by \'SR\' Gameing \n\n                    \"Guess the number\"\n")
+print("\nyou'r guessing number is  under \"0 to 100\" \n")
 print("                 \"Your Total life is  (9)\"")
 print("")
-NUM = 18
+NUM = random.randint(0,100)
 I=0
 while(True):
+      if(I==9):
+             nul = input("\npress any key\n")
+             print("\nGame Over Please try again \nThe and is ",NUM)
+             break
       print("\nyour life remaing:-",9-I)
       I=I+1
-      if(I==10):
-             print("\nGame Over Please try again \nThe and is ",NUM)
-             break       
+             
       num = int(input("\nEnter Guess Number:- "))
       if(NUM>num):
             print("\nWrong Answer! you answer is less then guess the number \nPlease  Increase your Number")
       if(NUM<num):
              print("Wrong Answer! you answer is Garter  then guess the number \nPlease Decrease you Number")
       if(num == NUM):
-            print("\n🎉🎉🎉🎉congratulation! you won 🎉🎉🎉🎉")
+            print("\n\n     🎉🎉🎉🎉🎉🎉congratulation!!! you won 🎉🎉🎉🎉🎉🎉")
             break
